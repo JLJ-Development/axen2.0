135dbc89b8d091b7371cc76a911abf6090bd4fca
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-03-15 08:17:57 +0700

    Create approvebot.js

---
+++ LICENSE.md/commands/approvebot.js
@@ -0,0 +1,18 @@
+/*
+const Discord = require('discord.js');
+
+module.exports = {
+  name: "approvebot",
+
+  async run(client, message, args) {
+    const eeembed = new Discord.MessageEmbed()
+    eeembed.setTitle("Mention A Bot Lol")
+    eeembed.setColor("RED")
+
+    const eembed = new Discord.MessageEmbed()
+    eembed.setTitle("Provide The UserID")
+
+    members.roles.add
+  }
+}
+*/
